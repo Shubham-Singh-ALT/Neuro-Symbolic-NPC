7b4512d91065f1b8ce973eaf3951e79d58cd916d
Shubham-Singh-ALT <shubhamsingh347567@gmail.com>
2026-04-01 19:18:18 +0530

    Update npc_env.py

--- npc_env.py
+++ npc_env.py
@@ -4,14 +4,13 @@ class SmartNPCEnv:
     def __init__(self):
         self.grid_size = 5
         self.reset()
-        # Actions: 0=Up, 1=Down, 2=Left, 3=Right
         self.action_space = [0, 1, 2, 3]
 
     def reset(self):
         """Resets the environment to the starting state."""
-        self.agent_pos = [0, 0]  # Top-left
-        self.goal_pos = [4, 4]   # Bottom-right
-        self.obstacles = [[1, 2], [2, 2], [3, 2]] # A wall in the middle
+        self.agent_pos = [0, 0] 
+        self.goal_pos = [4, 4]   
+        self.obstacles = [[1, 2], [2, 2], [3, 2]] 
         self.done = False
         return tuple(self.agent_pos)
 
@@ -20,24 +19,22 @@ class SmartNPCEnv:
         if self.done:
             return tuple(self.agent_pos), 0, True
 
-        # Calculate new position
         new_pos = list(self.agent_pos)
-        if action == 0 and new_pos[0] > 0:            new_pos[0] -= 1 # Up
-        elif action == 1 and new_pos[0] < self.grid_size - 1: new_pos[0] += 1 # Down
-        elif action == 2 and new_pos[1] > 0:            new_pos[1] -= 1 # Left
-        elif action == 3 and new_pos[1] < self.grid_size - 1: new_pos[1] += 1 # Right
+        if action == 0 and new_pos[0] > 0:            new_pos[0] -= 1 
+        elif action == 1 and new_pos[0] < self.grid_size - 1: new_pos[0] += 1 
+        elif action == 2 and new_pos[1] > 0:            new_pos[1] -= 1 
+        elif action == 3 and new_pos[1] < self.grid_size - 1: new_pos[1] += 1 
 
         self.agent_pos = new_pos
 
-        # Check conditions and assign rewards
         if self.agent_pos == self.goal_pos:
             self.done = True
-            reward = 10  # Found the goal!
+            reward = 10  
         elif self.agent_pos in self.obstacles:
             self.done = True
-            reward = -10 # Hit a wall!
+            reward = -10
         else:
-            reward = -1  # Small penalty to encourage the shortest path
+            reward = -1  
 
         return tuple(self.agent_pos), reward, self.done
 
@@ -58,7 +55,6 @@ class SmartNPCEnv:
         print(grid)
         print("-" * 20)
 
-# Quick test to see the grid
 if __name__ == "__main__":
     env = SmartNPCEnv()
     print("Initial State:")
